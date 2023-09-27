c5f1f2ad6aa8ec47967fbf5912eba650923de5f2
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2023-09-27 20:22:46 +0500

    Create Sort_0_1.cxx

---
+++ DSA & Problem Solving/Array/Sort_0_1.cxx
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void swaap(int *arr, int size)
+{
+  int  L = 0 , R = size-1; 
+
+    while (L<R)
+    {
+       while (arr[L]==0 &L<R)
+       {
+        L++;
+       }
+       while (arr[R]==1 && L<R)
+       {
+        R--;
+       }
+       // yahn pr loop a gy toh mtlb arr[l]==1 ha ior arr[R]==0 ha to humy in ko swap kr dana ah 
+       swap(arr[L],arr[R]);
+       L++;
+       R--;
+    }
+    
+}
+
+void print(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[5] = {1, 0, 0, 1, 0};
+    // sort(arr,arr+5); this can solve this but i am learning manual method
+
+    swaap(arr, 5);
+    print(arr, 5);
+
+    return 0;
+}
