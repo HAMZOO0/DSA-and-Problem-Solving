61eb0be36183b9db6a8153751a46736920ec6c48
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2023-10-10 12:42:58 +0500

    Create Selection_Sort__(new).cxx

---
+++ DSA & Problem Solving/Algorithm/Selection Sort/Selection_Sort__(new).cxx
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void srt(int *arr)
+{	
+	for(int i = 0 ; i < 4 ; i++)
+	{
+		int index  = i ; 
+		for(int j = i+1 ; j<5 ; j++ )
+		{
+			if(arr[j]<arr[index])
+				index = j;	  
+		}
+			swap(arr[index] , arr[i]);
+	}
+}
+
+void print (int *arr)
+{
+	for(int i = 0 ; i <5 ; i++)
+	{
+	cout<<arr[i]<<" ";
+    }
+}
+
+int main()
+{	
+	int arr[5]={2,6,1,7,8}; 
+	
+	srt(arr);
+	print (arr);
+		
+}
+
+/*
+
+ 1st iteration  == { 1 ,  6 , 2 , 7 , 8 }
+ 2nd  ======        {1,2,6,7,8}
+
+
+*/
