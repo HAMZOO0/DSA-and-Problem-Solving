8a9e8cc88a3e36f15126968df76afb06f63cb8e8
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2023-10-21 12:37:43 +0500

    Update insertio_sort.cxx

--- DSA & Problem Solving/Algorithm/Insertion Sort/insertio_sort.cxx
+++ DSA & Problem Solving/Algorithm/Insertion Sort/insertio_sort.cxx
@@ -2,6 +2,14 @@
 Hamza
 21-10-23
 insertion sort
+
+Time complexity
+Best case time complexity is : O(n) 
+worst case Time  complexity is : O(n^2)
+
+Space complexity
+O(1) 
+
 */
 
 #include <iostream>
@@ -12,20 +20,21 @@ int main(int argc, char const *argv[])
     int n = 7;
     int arr[7] = {10, 1, 7, 4, 8, 2, 11};
 
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < n; i++) // this loop is use to select a elements
     {
         int temp = arr[i];
 
         int j = i - 1;
-        for (; j >= 0; j--)
+        for (; j >= 0; j--)     // this loop use to compare the selected (i) element with i-1 elemts 
         {
-            if (arr[j] > temp)
-                arr[j + 1] = arr[j];
+            if (arr[j] > temp)   // if next (j) elemnts is less then previus one then we shift 
+                arr[j + 1] = arr[j];   // |10|10|7| ...    temp =1 
 
             else
                 break;
         }
-        arr[j + 1] = temp;
+        arr[j + 1] = temp;          //after that j value alwas like (j-1)
+        //cout<<j<<" "; 
 
 
     }
