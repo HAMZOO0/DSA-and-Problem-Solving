0f94c0be575027e77c7fa42dc76eb09c092b969e
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2023-11-20 16:48:22 +0500

    Create base_Recursion .cxx

---
+++ DSA & Problem Solving/Methods/base_Recursion .cxx
@@ -0,0 +1,9 @@
+* Recursion  : function call itself + big problem solution depends on same type of small problem 
+ex : factorial = 5! = 5 x 4!  --------- 1! = 1 * 0 --> base case "ruk jao" 
+f(n) = n * f(n-1 )
+
+  * In Recursion  Base Case is must . use for stop the program
+return is mandatry in Recursion
+
+
+//////////////////////////////////// Simple program to learn about function call 
