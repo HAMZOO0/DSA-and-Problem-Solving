a36a60fb3be3e8dfecb796687c3d2b71afc27865
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2023-12-23 23:22:23 +0500

    Update zero_counter.cxx

--- DSA & Problem Solving/Methods/Recursion/Problems/zero_counter.cxx
+++ DSA & Problem Solving/Methods/Recursion/Problems/zero_counter.cxx
@@ -1,30 +1,47 @@
+// in this program i write 2 methods
+// method # 1 
+
+int sum = 0;
+int fun(int n)
+{
+  if (n == 0)
+    return sum;
+
+  if(n%10 == 0 )
+  sum++ ; 
+
+  return fun(n / 10);
+}
+
+int main(int argc, char const *argv[])
+{
+  int n1 = 1001;
+  cout<< fun(n1);
+}
+//////////////////////////////////////////////////////////////////////
+
+// method # 2 
+
 #include <iostream>
 using namespace std;
 
-int counter = 0 ; 
-
-int count (int n )
+int fun(int n, int count)
 {
-    if ( n <1 )
-    {
-        return counter; 
-    } 
+  if (n == 0)
+return count;
+
 
     if (n % 10 == 0)
-    {
-      counter += 1;
-    }
+      return fun(n / 10, ++count);
 
-    return count(n/10);
-
+    else
+      return fun(n / 10, count);
 }
 
-int main()
+int main(int argc, char const *argv[])
 {
-  int n = 10;
- 
-
-  cout << count(n);
+  int n1 = 1001;
+  cout << fun(n1, 0);
 
   return 0;
 }
