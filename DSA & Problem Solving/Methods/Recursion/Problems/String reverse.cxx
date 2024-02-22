4355833eb64e3a4df3bd2bd1d4cd60245ebdee66
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2024-02-22 03:06:10 -0800

    Update String reverse.cxx

--- DSA & Problem Solving/Methods/Recursion/Problems/String reverse.cxx
+++ DSA & Problem Solving/Methods/Recursion/Problems/String reverse.cxx
@@ -1,3 +1,5 @@
+// Two pointer 
+
 #include<iostream>
 using namespace std;
 
@@ -17,6 +19,30 @@ int main(){
 	string st = "abcd";
 	
 	rev(0 , st.length()-1, st) ; 
+	
+	cout<<st;
+}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// one pointer 
+
+#include<iostream>
+using namespace std;
+
+void rev(int i  , string &str)
+{
+	if(i>str.length()-1-i)
+	return;
+	
+	swap(str[i],str[str.length()-1-i]);
+	i++;
+	
+	rev(i , str);
+	
+}
+int main(){
+	string st = "abcs";
+	
+	rev(0 , st) ; 
 	
 	cout<<st;
 }
