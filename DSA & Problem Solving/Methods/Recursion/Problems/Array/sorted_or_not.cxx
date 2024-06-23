8f2825cf9be3db107f196c2eae0430590b34d57b
Hamza Sajid <hamzakhan174722@gmail.com>
2024-06-23 22:32:28 +0500

    Create sorted_or_not.cxx

---
+++ DSA & Problem Solving/Methods/Recursion/Problems/Array/sorted_or_not.cxx
@@ -0,0 +1,38 @@
+/*
+Hamza
+23
+topic: recursion
+*/
+#include <iostream>
+using namespace std;
+bool foo(int *arr, int start, int end)
+{
+    if (start == end)
+    {
+        return true;
+    }
+
+    else if (arr[start] > arr[start + 1])
+    {
+        return false;
+    }
+
+    start++;
+    return foo(arr, start, end);
+}
+int main(int argc, char const *argv[])
+{
+    int arr[] = {1, 2, 1, 4};
+
+    bool check = foo(arr, 0, 3);
+
+    if (check)
+    {
+        cout << "sorted";
+    }
+
+    else
+        cout << "not";
+
+    return 0;
+}
