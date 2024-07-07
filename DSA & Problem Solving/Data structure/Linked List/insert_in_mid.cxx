94b793d481efd3b6de9d2af11d39b6d876811321
Hamza Sajid <hamzakhan174722@gmail.com>
2024-07-07 23:17:19 +0500

    Create insert_in_mid.cxx

---
+++ DSA & Problem Solving/Data structure/Linked List/insert_in_mid.cxx
@@ -0,0 +1,76 @@
+/*
+Hamza
+23
+topic:
+*/
+#include <iostream>
+using namespace std;
+
+class Node
+{
+
+public:
+    int data;
+    Node *Next_node;
+
+    Node(int val) : data(val), Next_node(nullptr) {}
+};
+
+void insert_in_tail(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    if (head == NULL)
+    {
+        // temp->Next_node = head;
+        head = temp;
+        return;
+    }
+
+    Node *tail = head;
+    while (tail->Next_node != NULL)
+    {
+        tail = tail->Next_node;
+    }
+
+    tail->Next_node = temp;
+}
+void insert_in_mid(Node *&head, int data, int postion)
+{
+    Node *NEW_NODE_TO_INSert = new Node(data);
+    Node *temp = head;
+    int count = 1;
+
+    while (count <= postion - 1)
+    {
+        temp = temp->Next_node;
+        ++count;
+    }
+
+    NEW_NODE_TO_INSert->Next_node = temp->Next_node;
+
+    temp->Next_node = NEW_NODE_TO_INSert;
+}
+
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " " << head->Next_node << endl;
+        head = head->Next_node;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    Node *head = nullptr; // Initialize an empty list
+
+    // Insert elements at the head
+    insert_in_tail(head, 2);
+    insert_in_tail(head, 22);
+    insert_in_tail(head, 202);
+
+    insert_in_mid(head, 1000, 2);
+
+    print(head);
+    return 0;
+}
