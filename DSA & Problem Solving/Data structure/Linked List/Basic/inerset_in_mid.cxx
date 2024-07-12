b3205b44e0ac8d3293c2e16a4fa9cbd07f8d61cf
Hamza Sajid <hamzakhan174722@gmail.com>
2024-07-12 23:16:16 +0500

    Create inerset_in_mid.cxx

---
+++ DSA & Problem Solving/Data structure/Linked List/Basic/inerset_in_mid.cxx
@@ -0,0 +1,27 @@
+void insert_in_mid(Node *&head, int data, int postion)
+{
+    if (postion == 1)
+    {
+        // call head_insert_function
+    }
+
+    Node *NEW_NODE_TO_INSert = new Node(data);
+    Node *temp = head;
+    int count = 1;
+
+    while (count < postion - 1)
+    {
+        temp = temp->Next_node;
+        ++count;
+    }
+
+    if (temp == NULL)
+    {
+        cout << "invalid position";
+    }
+    NEW_NODE_TO_INSert->Next_node = temp->Next_node;
+
+    NEW_NODE_TO_INSert->previous_node = temp;
+
+    temp->Next_node = NEW_NODE_TO_INSert;
+}
